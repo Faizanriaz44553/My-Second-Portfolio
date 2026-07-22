9941443160b60084092d72aff131d2452cf16d7d
Faizanriaz44553 <sylanipractice@gmail.com>
2026-07-22 15:34:55 -0700

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: [
+      '@babel/runtime/helpers/createSuper',
+      'react-swipeable-views-react-18-fix',
+    ],
+  },
+  build: {
+    chunkSizeWarningLimit: 1600, // Warning threshold badhane ke liye
+    commonjsOptions: {
+      include: [/@babel\/runtime/, /node_modules/],
+    },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('@mui') || id.includes('@emotion')) return 'mui-vendor';
+            if (id.includes('framer-motion') || id.includes('gsap')) return 'animation-vendor';
+            if (id.includes('firebase')) return 'firebase-vendor';
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
